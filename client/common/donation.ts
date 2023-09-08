c3fb01d86098154d723c0a26cb239b25fb5b0c6d
Sacha Beaumier <sacha.beaumier@gmail.com>
2023-09-08 19:47:13 +1200

    server, routes, migrations and seeds completed

---
+++ client/common/donation.ts
@@ -0,0 +1,12 @@
+export interface Donation {
+  id?: number
+  political_party?: string
+  date_received?: number
+  donor?: string
+  address?: string
+  lat?: number
+  lon?: number
+  total?: number
+  sector?: string
+  description?: string
+}
--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import path from 'path'
+import donations from './routes/donations'
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
+
+server.use('/api/v1/2023/donations', donations)
 
 export default server
