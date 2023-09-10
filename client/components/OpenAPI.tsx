db5309731287933575fc4d4171418569ad0a847f
Sacha Beaumier <sacha.beaumier@gmail.com>
2023-09-10 20:06:13 +1200

    fixing api endpoint

--- client/components/OpenAPI.tsx
+++ client/components/OpenAPI.tsx
@@ -6,7 +6,10 @@ export default function OpenAPI() {
         <p>Access to this data is available as an open API endpoint</p>
         <p>
           The api is available at link{' '}
-          <a href="http://localhost:3000/api/v1/2023/donations">here</a>
+          {/* <a href="http://localhost:3000/api/v1/2023/donations">here</a> */}
+          <a href="https://nzelectoraldonationstracker2023-production.up.railway.app/api/v1/2023/donations">
+            here
+          </a>
         </p>
       </div>
     </>
